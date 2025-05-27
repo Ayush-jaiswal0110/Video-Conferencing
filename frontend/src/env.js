316b603cd6f4f925c2dff0d00194b40bf75d9874
Ayush0110 <ayushjaiswalji6@gmail.com>
2025-05-27 21:32:38 +0530

    solved error

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
-const servers = IS_PROD ?
+const server = IS_PROD ?
     "http://localhost:8000": 
-    
+
      "https://video-conferencing-backend-carn.onrender.com"
 
 
-export default servers;
+export default server;
